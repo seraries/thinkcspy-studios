bad4424a87fc8a1fbd1dcaf1defed3d12ef83a30
Sarah Richardson <seraries@gmail.com>
2017-07-07 16:57:32 -0500

    avoid variable name starting with lowercase l

--- bubble-sort/studio-solution.py
+++ bubble-sort/studio-solution.py
@@ -5,16 +5,16 @@ Does not alter the original list but returns a sorted version of it.
 
     is_sorted = False
     while is_sorted == False:
-        nswaps = 0
-        for i in range(0, len(lst) - 1):
-            a = lst[i]
-            b = lst[i+1]
+        num_swaps = 0
+        for i in range(0, len(a_list) - 1):
+            a = a_list[i]
+            b = a_list[i+1]
             if a > b:
-                lst[i] = b
-                lst[i+1] = a
-                nswaps = nswaps + 1
-        if nswaps == 0:
+                a_list[i] = b
+                a_list[i+1] = a
+                num_swaps = num_swaps + 1
+        if num_swaps == 0:
             is_sorted = True
-    return lst
+    return a_list
 
 bubble_sort([3,2,1])
